bf31d7f480490a62bce6b0f2c3c837890f04972e
Hélène CARRIOU <helenec56@laposte.net>
2022-06-02 14:32:41 +0200

    regex html psw + pseudo

--- App/Views/admin/connexion/createAdmin.php
+++ App/Views/admin/connexion/createAdmin.php
@@ -6,9 +6,9 @@
     <h2>Créer un nouvel administrateur</h2>
 
     <form action="indexAdmin.php?action=createAdminPost" method="post" enctype='multipart/form-data'>
-        <p><input type="text" name="adminPseudo" id="pseudo" placeholder="Votre pseudo"></p>
+        <p><input type="text" pattern="^[a-zA-Z0-9]{5,}$" title="Votre pseudo doit contenir au moins 5 caractère et n'accepte pas les caractères spéciaux" name="adminPseudo" id="pseudo" placeholder="Votre pseudo"></p>
         <p><input type="email" name="adminMail" id="mail" placeholder="Votre adresse e-mail" value=""></p>
-        <p><input type="password" name="adminMdp" id="adminMdp" placeholder="Votre mot de passe" value=""></p>
+        <p><input type="password" pattern="^(?=.{7,})(?=.*[a-z])(?=.*[A-Z])(?=.*[!£@$%^&*()_+=\-`{}:~#';<>?/.,|\\]).*$" title="Votre mot de passe doit contenir au moins 7 caractères, une minuscule, une majuscule et un caractère spécial" name="adminMdp" id="adminMdp" placeholder="Votre mot de passe" value=""></p>
         <p><input type="file" name="picture" id="inputImg" accept="image/*"></p>
         <input type="submit">
     </form>
--- App/Views/front/user-create.php
+++ App/Views/front/user-create.php
@@ -7,8 +7,8 @@
 
     <form action="indexAdmin.php?action=createUserPost" method="post" enctype='multipart/form-data'>
         <p><input type="text" name="userPseudo" id="pseudo" placeholder="Votre pseudo"><span class="required">*</span></p>
-        <p><input type="email" name="userMail" id="mail" placeholder="Votre adresse e-mail"><span class="required">*</span></p>
-        <p><input type="password" name="userMdp" id="userMdp" placeholder="Votre mot de passe"><span class="required">*</span></p>
+        <p><input type="email" name="userMail" id="mail" placeholder="Votre adresse e-mail" pattern="^[a-zA-Z0-9]{5,}$" title="Votre pseudo doit contenir au moins 5 caractère et n'accepte pas les caractères spéciaux"><span class="required">*</span></p>
+        <p><input type="password" name="userMdp" id="userMdp" placeholder="Votre mot de passe" pattern="^(?=.{7,})(?=.*[a-z])(?=.*[A-Z])(?=.*[!£@$%^&*()_+=\-`{}:~#';<>?/.,|\\]).*$" title="Votre mot de passe doit contenir au moins 7 caractères, une minuscule, une majuscule et un caractère spécial"><span class="required">*</span></p>
         <p><input type="file" name="picture" id="inputImg" accept="image/*"></p>
         <button class="btn" type="submit">Créer mon compte</button>
     </form>
