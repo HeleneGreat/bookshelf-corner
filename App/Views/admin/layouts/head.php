43e24e14b19e9a7b592aac4dc2d7792c1b1408d4
Hélène CARRIOU <helenec56@laposte.net>
2022-06-15 12:03:09 +0200

    SEO + performances improved

--- App/Views/admin/layouts/head.php
+++ App/Views/admin/layouts/head.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#3786ed">
+    <meta name="description" content="<?= $blog['name']; ?> c'est le lieu de rendez-vous de tous les amoureux de la litérature. Bienvenue sur votre espace utilisateur, pour participer activement à la vie du blog !">
     <title><?= $currentPageTitle; ?> | Espace <?= $_SESSION['role'] == 0 ? "utilisateur" : "admin"; ?> | <?= $blog['name']; ?></title>
     <link rel="icon" type="image/x-icon" href="./App/Public/Front/images/puzzle-back.png">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
--- App/Views/front/layouts/head.php
+++ App/Views/front/layouts/head.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="theme-color" content="#ee5f4f">
     <meta name="description" content="<?= $blog['name']; ?> c'est le lieu de rendez-vous de tous les amoureux de la litérature. Venez nous rejoindre à la découverte de nouvelles pépites, et participez activement à la vie du blog grâce à vos commentaires et vos discussions entre passionnés !">
     <title><?=$currentPageTitle; ?> | <?= $blog['name']; ?></title>
     <link rel="icon" type="image/x-icon" href="./App/Public/Front/images/puzzle-front.png">
